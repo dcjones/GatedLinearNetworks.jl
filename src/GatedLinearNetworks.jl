5fcd2956a28923eff5504120a2674f5c06d0174c
Daniel C. Jones <dcjones@cs.washington.edu>
2021-01-30 18:23:26 -0800

    Work the bugs out of the broadcasting.

--- src/GatedLinearNetworks.jl
+++ src/GatedLinearNetworks.jl
@@ -3,45 +3,6 @@ module GatedLinearNetworks
 using Flux
 
 
-"""
-A "context function" is just a locality sensitive hash function that maps
-normalized data vectors to integer indexes.
-"""
-struct ContextFunction{
-        M <: AbstractMatrix{<:Real},
-        V <: AbstractVector{<:Real},
-        VI <: AbstractVector{22}}
-    C::M # [context dim, data dim]
-    b::V # [context dim]
-    idx::VI # [context dim]
-end
-
-
-"""
-Produce a new random context function where `d` is the dimensionality of the
-data and `c` the context dimensionality.
-"""
-function ContextFunction(T::Real, d::Int, c::Int)
-    C = rand(T, (c, d))
-    C ./= sum(C.^2, dims=1)
-
-    b = rand(T, c)
-
-    idx = collect(Int32(0):Int32(c-1))
-
-    return ContextFunction(C, b, idx)
-end
-
-
-"""
-Apply the context function to data in X, where size(X, 1) is the number of
-features, and size(X, 2) is the number of items in the batch.
-"""
-function (f::ContextFunction{M})(X::M)
-    sum((f.C * X .> b) .<< F.idx, dims=1)
-end
-
-
 # TODO: implemnent online mean std estimation
 # I think the idea is that we standardize input is it comes to us, then
 # feed that into the context functions.
@@ -63,10 +24,10 @@ struct BasicGaussianLinearRegression <: GaussianWeakLearner
 end
 
 
-function forward(bglr::BasicGaussianLinearRegression, X::M, train::Bool) where
-        {M<:AbstractMatrix{<:Real}}
+# function forward(bglr::BasicGaussianLinearRegression, X::M, train::Bool) where
+#         {M<:AbstractMatrix{<:Real}}
 
-end
+# end
 
 
 
@@ -83,7 +44,8 @@ struct GGLNLayer{
     input_dim::Int
     output_dim::Int
     context_dim::Int
-    data_dim::Int
+    predictor_dim::Int
+    prediction_dim::Int
 
     learning_rate::Float64
 
@@ -100,22 +62,25 @@ end
 
 
 function GGLNLayer(
-        input_dim::Int, output_dim::Int, context_dim::Int, data_dim::Int,
+        input_dim::Int, output_dim::Int,
+        context_dim::Int, predictor_dim::Int, prediction_dim::Int,
         learning_rate::Float64=1e-3)
-    hyperplanes = randn(T, (output_dim*context_dim, data_dim))
-    hyperplanes_bias = randn(T, (output_dim*context_dim))
+    hyperplanes = randn(Float32, (output_dim*context_dim, predictor_dim))
+    hyperplanes_bias = randn(Float32, (output_dim*context_dim))
 
-    bit_offsets = collect(Int32, 0:contains-1)
+    bit_offsets = collect(Int32, 0:context_dim-1)
     k_offsets = collect(Int32, 1:output_dim)
 
+    # TODO:
     # How do I initialize weights? They are in some hypercube, right?
-    weights = randn(T, (input_dim, output_dim*(2^context_dim)))
+    weights = exp.(randn(Float32, (input_dim, output_dim*(2^context_dim))))
 
     return GGLNLayer(
         input_dim,
         output_dim,
         context_dim,
-        data_dim,
+        predictor_dim,
+        prediction_dim,
         learning_rate,
         hyperplanes,
         hyperplanes_bias,
@@ -125,8 +90,12 @@ function GGLNLayer(
 end
 
 
+"""
+Apply the context functions for this layer, mapping (standardized) input vectors
+to indexes in [1, 2^context_dim].
+"""
 function context_functions(layer::GGLNLayer, X::AbstractMatrix)
-    bits = (layer.hyperplanes_bias * X) .> layer.hyperplanes_bias
+    bits = (layer.hyperplanes * X) .> layer.hyperplanes_bias
     batch_dim = size(X, 2)
     bits_reshape = reshape(bits, (layer.context_dim, layer.output_dim, batch_dim))
     cs = dropdims(sum(bits_reshape .<< layer.bit_offsets, dims=1), dims=1) .+ layer.k_offsets
@@ -135,50 +104,59 @@ function context_functions(layer::GGLNLayer, X::AbstractMatrix)
 end
 
 
-
 """
 
-y should be []
+input_μ: [input_dim, prediction_dim, batch_dim]
+input_σ2: [input_dim, prediction_dim, batch_dim]
+z: [predictor_dim, batch_dim]
+y: [prediction_dim, batch_dim]
 """
 function apply(
-        layer::GGLNLayer, input_μ_::AbstractMatrix, input_σ2_::AbstractMatrix,
-        y::AbstractMatrix, training::Bool)
+        layer::GGLNLayer, input_μ::AbstractArray, input_σ2::AbstractArray,
+        z::AbstractMatrix, y::AbstractMatrix, training::Bool)
 
     # input_μ, input_σ should be [input_dim, batch_dim]
-    batch_dim = size(μ, 2)
+    batch_dim = size(input_μ, 3)
 
     # [output_dim, batch_dim]
-    cs = context_functions(layer, X)
+    cs = context_functions(layer, z)
 
-    # [input_dim, output_dim, batch_dim]
+    # [input_dim, output_dim, 1, batch_dim]
     weights = layer.weights[:,cs]
 
-    input_μ = reshape(input_μ_, (layer.input_dim, 1, batch_dim))
-    input_σ2 = reshape(input_σ2_, (layer.input_dim, 1, batch_dim))
-
     function predict(weights)
-        # [output_dim, batch_dim]
-        σ2 = inv.(dropdims(sum(weights ./ input_σ2, dims=1), dims=1))
+        weights_ = reshape(
+            weights, (layer.input_dim, layer.output_dim, 1, batch_dim))
 
-        # [dim, batch_dim]
-        μ = σ2 .* dropdims(sum(weights .* input_μ ./ input_σ2, dims=1), dims=1)
+        # [input_dim, 1, prediction_dim, batch_dim]
+        input_μ_ = reshape(input_μ, (layer.input_dim, 1, layer.prediction_dim, batch_dim))
+        input_σ2_ = reshape(input_σ2, (layer.input_dim, 1, layer.prediction_dim, batch_dim))
+
+        # [output_dim, prediction_dim, batch_dim]
+        σ2 = inv.(dropdims(sum(weights_ ./ input_σ2_, dims=1), dims=1))
+
+        # [output_dim, prediction_dim, batch_dim]
+        μ = σ2 .* dropdims(sum(weights_ .* input_μ_ ./ input_σ2_, dims=1), dims=1)
 
         return μ, σ2
     end
 
     function loss(weights)
+        # [output_dim, prediction_dim, batch_dim]
         μ, σ2 = predict(weights)
         σ = sqrt.(σ2)
 
-        # TODO:
-        # Now, I'm perplexed. I think μ should have an extra dimension, Since
-        # the output dimension can be higher.
+        @show size(μ)
+        @show size(y)
 
-        return -sum(.- log.(σ) .- 0.5 * log.(2*π) .- 0.5 .* ((y - μ) / σ)^2)
+        y_ = reshape(y, (1, layer.prediction_dim, batch_dim))
+
+        # negative normal log-pdf
+        return -sum(.- log.(σ) .- 0.5 * log.(2*π) .- 0.5 .* ((y_ .- μ) ./ σ).^2)
     end
 
     if training
-        dloss_dweights = gradient(loss, weights)
+        dloss_dweights = gradient(loss, weights)[1]
         layer.weights[:,cs] = weights - layer.learning_rate * dloss_dweights
     end
 
